6401d3116a35c7d507bdf6e77589ffc9c0e9255e
Xianhua Liu <xl24@duke.edu>
2010-03-02 18:20:24 +0000

    changed recaptcha verifying server to its IP address

--- vendor/plugins/recaptcha/lib/recaptcha.rb
+++ vendor/plugins/recaptcha/lib/recaptcha.rb
@@ -14,7 +14,8 @@ module Recaptcha
   
   RECAPTCHA_API_SERVER        = 'http://api.recaptcha.net';
   RECAPTCHA_API_SECURE_SERVER = 'https://api-secure.recaptcha.net';
-  RECAPTCHA_VERIFY_SERVER     = 'api-verify.recaptcha.net';
+  RECAPTCHA_VERIFY_SERVER     = '69.12.97.166'; 
+  #'api-verify.recaptcha.net';
 
   SKIP_VERIFY_ENV = ['test', 'cucumber']
 
